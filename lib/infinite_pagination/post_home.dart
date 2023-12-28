75284e540ac16147aa8c50e363f7916212171a45
Pikiiiuuu <fiqiandrireviansyah@gmail.com>
2023-12-28 19:20:39 +0700

    Add files of ininite_scroll of app version including fetching 10 data per page

--- lib/infinite_pagination/post_home.dart
+++ lib/infinite_pagination/post_home.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lyg_test/infinite_pagination/model.dart';
+import 'package:flutter_lyg_test/ly_product_detail.dart';
 import 'package:flutter_lyg_test/size_config.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-
+//files of infinite_scroll of app version
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -18,14 +19,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //initiate the amount of data per page
   static const int _perPage = 10;
 
   final oCcy = new NumberFormat("#,##0", "en_US");
 
+  //initiate the first of page
   final PagingController<int, Product> _pagingController = PagingController(
     firstPageKey: 0,
   );
 
+  //fetching data
   Future<void> _getProducts(int page) async {
     print(page);
     final client = http.Client();
@@ -76,140 +80,160 @@ class _MyHomePageState extends State<MyHomePage> {
                   pagingController: _pagingController,
                   builderDelegate: PagedChildBuilderDelegate<Product>(
                     transitionDuration: const Duration(seconds: 2),
-                    itemBuilder: (_, item, __) => Card(
-                      child: Column(
-                        children: [
-                          item.thumbnail == null
-                              ? AspectRatio(
-                            aspectRatio: 3,
-                            child: Container(
-                              padding: EdgeInsets.all(getProportionateScreenWidth(5)),
+                    itemBuilder: (_, item, __) => InkWell(
+                      onTap: (){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsScreen(
+                                  images: item.images,
+                                  discountPercentage:
+                                  item.discountPercentage.toString(),
+                                  title: item.discountPercentage,
+                                  description: item.description,
+                                  price: item.price.toString(),
+                                  rating: '5',
+                                  stock: item.stock.toString(),
+                                  brand: item.brand,
+                                  category: item.category,
+                                  thumbnail: item.thumbnail),
+                            ));
+                      },
+                      child: Card(
+                        child: Column(
+                          children: [
+                            item.thumbnail == null
+                                ? AspectRatio(
+                              aspectRatio: 3,
                               child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: getProportionateScreenHeight(100),
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(
-                                        "${item.thumbnail}"),
+                                padding: EdgeInsets.all(getProportionateScreenWidth(5)),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: getProportionateScreenHeight(100),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          "${item.thumbnail}"),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          )
-                              : Stack(
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 1.00,
-                                child: Container(
-                                  padding: EdgeInsets.all(getProportionateScreenWidth(5)),
+                            )
+                                : Stack(
+                              children: [
+                                AspectRatio(
+                                  aspectRatio: 1.00,
                                   child: Container(
-                                    width: MediaQuery.of(context).size.width,
-                                    height: getProportionateScreenHeight(100),
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: NetworkImage(
-                                            "${item.thumbnail}"),
+                                    padding: EdgeInsets.all(getProportionateScreenWidth(5)),
+                                    child: Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      height: getProportionateScreenHeight(100),
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                              "${item.thumbnail}"),
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Positioned(
-                                top: 1,
-                                right: 0,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white.withOpacity(0.8),
-                                    shape: BoxShape.rectangle,
-                                    border: Border.all(
-                                      color: Colors.transparent,
+                                Positioned(
+                                  top: 1,
+                                  right: 0,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.8),
+                                      shape: BoxShape.rectangle,
+                                      border: Border.all(
+                                        color: Colors.transparent,
+                                      ),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.3),
+                                          offset: new Offset(0.0, 0.0),
+                                          blurRadius: 1.0,
+                                          spreadRadius: 0.0,
+                                        ),
+                                      ],
                                     ),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(0.3),
-                                        offset: new Offset(0.0, 0.0),
-                                        blurRadius: 1.0,
-                                        spreadRadius: 0.0,
+                                    child: Padding(
+                                        padding: const EdgeInsets.all(0.5),
+                                        child: Text("Stock ${item.stock}", style: GoogleFonts.poppins(fontSize: 12),)
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Divider(),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10, right: 10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${item.title} | ${item.brand}",
+                                    style: GoogleFonts.lato(color: Colors.black),
+                                    maxLines: 2,
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '\$ ${oCcy.format(double.parse('${item.price}'))}',
+                                        style: GoogleFonts.lato(
+                                            fontWeight:
+                                            FontWeight
+                                                .bold,
+                                            fontSize: 18),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '(${item.discountPercentage} %)',
+                                        style: TextStyle(
+                                            color: Colors
+                                                .green,
+                                            fontWeight:
+                                            FontWeight
+                                                .bold,
+                                            fontSize: 12),
                                       ),
                                     ],
                                   ),
-                                  child: Padding(
-                                      padding: const EdgeInsets.all(0.5),
-                                      child: Text("Stock ${item.stock}", style: GoogleFonts.poppins(fontSize: 12),)
-                                  ),
-                                ),
+                                  // Row(
+                                  //   children: [
+                                  //     Icon(
+                                  //       Icons.store,
+                                  //       color: kPrimaryColor,
+                                  //       size: 15,
+                                  //     ),
+                                  //     SizedBox(
+                                  //       width: 3,
+                                  //     ),
+                                  //     Text(
+                                  //       "${_get[index]['createdBy'].length > 25 ? _get[index]['createdBy'].substring(0, 25) + '...' : _get[index]['createdBy']}",
+                                  //       maxLines: 1,
+                                  //       style: GoogleFonts.lato(
+                                  //           fontWeight: FontWeight.w600,
+                                  //           color: kPrimaryColor,
+                                  //           fontSize: getProportionateScreenWidth(10)),
+                                  //     )
+                                  //   ],
+                                  // ),
+                                  Divider(),
+                                  // StarRating(
+                                  //   rating: double.parse("${_found[index]['rating']}"),
+                                  //   // onRatingChanged: (rating) => setState(() => this._get[index]['rating'] = _get[index]['rating']),
+                                  // ),
+                                ],
                               ),
-                            ],
-                          ),
-                          Divider(),
-                          Padding(
-                            padding: EdgeInsets.only(left: 10, right: 10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "${item.title} | ${item.brand}",
-                                  style: GoogleFonts.lato(color: Colors.black),
-                                  maxLines: 2,
-                                ),
-                                SizedBox(height: 5,),
-                                Row(
-                                  children: [
-                                    Text(
-                                      '\$ ${oCcy.format(double.parse('${item.price}'))}',
-                                      style: GoogleFonts.lato(
-                                          fontWeight:
-                                          FontWeight
-                                              .bold,
-                                          fontSize: 18),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      '(${item.discountPercentage} %)',
-                                      style: TextStyle(
-                                          color: Colors
-                                              .green,
-                                          fontWeight:
-                                          FontWeight
-                                              .bold,
-                                          fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                                // Row(
-                                //   children: [
-                                //     Icon(
-                                //       Icons.store,
-                                //       color: kPrimaryColor,
-                                //       size: 15,
-                                //     ),
-                                //     SizedBox(
-                                //       width: 3,
-                                //     ),
-                                //     Text(
-                                //       "${_get[index]['createdBy'].length > 25 ? _get[index]['createdBy'].substring(0, 25) + '...' : _get[index]['createdBy']}",
-                                //       maxLines: 1,
-                                //       style: GoogleFonts.lato(
-                                //           fontWeight: FontWeight.w600,
-                                //           color: kPrimaryColor,
-                                //           fontSize: getProportionateScreenWidth(10)),
-                                //     )
-                                //   ],
-                                // ),
-                                Divider(),
-                                // StarRating(
-                                //   rating: double.parse("${_found[index]['rating']}"),
-                                //   // onRatingChanged: (rating) => setState(() => this._get[index]['rating'] = _get[index]['rating']),
-                                // ),
-                              ],
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                     firstPageErrorIndicatorBuilder: (context) => Column(
